ec1702fbc28c5b916d2e5ad05de56d56e65959dc
R <rqou@berkeley.edu>
2025-04-19 01:43:35 +0100

    Make button larger

--- 07_android/app/src/main/java/com/arcanenibble/demodice/MainActivity.kt
+++ 07_android/app/src/main/java/com/arcanenibble/demodice/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -20,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.arcanenibble.demodice.ui.theme.DemoDiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +43,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceApp(modifier: Modifier = Modifier) {
-    var diceRoll by remember { mutableStateOf(0) }
+    var diceRoll by remember { mutableIntStateOf(0) }
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -48,16 +51,18 @@ fun DiceApp(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center
     ) {
         Button(
+            modifier = Modifier.size(width = 200.dp, height = 200.dp),
             onClick = {
                 diceRoll = (1..20).random()
             }
         ) {
             Text(
-                if (diceRoll == 0) {
+                text = if (diceRoll == 0) {
                     stringResource((R.string.roll))
                 } else {
                     diceRoll.toString()
-                }
+                },
+                style = MaterialTheme.typography.titleLarge
             )
         }
     }
