5ddc5af87b1811f021f3505d48277f8457e51b6c
R <rqou@berkeley.edu>
2025-04-19 04:16:15 +0100

    Not quite working settings

--- 07_android/app/src/main/java/com/arcanenibble/demodice/MainActivity.kt
+++ 07_android/app/src/main/java/com/arcanenibble/demodice/MainActivity.kt
@@ -7,22 +7,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -31,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arcanenibble.demodice.ui.theme.DemoDiceTheme
@@ -50,15 +60,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DiceToplevel() {
     var showSettings by remember { mutableStateOf(false) }
+    var maxRoll by remember { mutableIntStateOf(20) }
 
     if (!showSettings) {
-        DiceMainApp(showSettings = {
-            showSettings = true
-        })
+        DiceMainApp(
+            showSettings = {
+                showSettings = true
+            },
+            maxRoll = {maxRoll},
+        )
     } else {
-        Settings(hideSettings = {
-            showSettings = false
-        })
+        SettingsDialog(
+            hideSettings = {
+                showSettings = false
+            },
+            setNewMaxRoll = {
+                maxRoll = it
+            }
+        )
     }
 }
 
@@ -66,7 +85,8 @@ fun DiceToplevel() {
 @OptIn(ExperimentalMaterial3Api::class)
 fun DiceMainApp(
     modifier: Modifier = Modifier,
-    showSettings: () -> Unit
+    showSettings: () -> Unit,
+    maxRoll: () -> Int = {20},
 ) {
     Scaffold(
         modifier = modifier.fillMaxSize(),
@@ -102,7 +122,10 @@ fun DiceMainAppPreview() {
 }
 
 @Composable
-fun DiceButton(modifier: Modifier = Modifier) {
+fun DiceButton(
+    modifier: Modifier = Modifier,
+    maxRoll: () -> Int = {20}
+) {
     var diceRoll by remember { mutableIntStateOf(0) }
 
     Column(
@@ -113,7 +136,7 @@ fun DiceButton(modifier: Modifier = Modifier) {
         Button(
             modifier = Modifier.size(width = 200.dp, height = 200.dp),
             onClick = {
-                diceRoll = (1..20).random()
+                diceRoll = (1..maxRoll()).random()
             }
         ) {
             Text(
@@ -137,39 +160,84 @@ fun DiceButtonPreview() {
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-fun Settings(
+fun SettingsDialog(
     modifier: Modifier = Modifier,
-    hideSettings: () -> Unit
+    hideSettings: () -> Unit,
+    setNewMaxRoll: (Int) -> Unit,
 ) {
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(stringResource(R.string.settings))
-                },
-                navigationIcon = {
-                    IconButton(onClick = hideSettings) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(R.string.back)
-                        )
-                    }
-                },
-            )
+    var maxRollStr by remember { mutableStateOf("") }
+    val maxRollIsValid by derivedStateOf {
+        val num = maxRollStr.toIntOrNull()
+        if (num == null) {
+            false
+        } else {
+            num > 0
         }
-    ) { innerPadding ->
-        Text("ASDF!", modifier = Modifier.padding(innerPadding))
     }
+
+    BasicAlertDialog(
+        onDismissRequest = hideSettings,
+        content = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(16.dp),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    OutlinedTextField(
+                        value = maxRollStr,
+                        onValueChange = { maxRollStr = it },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        label = { Text(stringResource(R.string.set_max)) },
+                        modifier = Modifier.padding(16.dp),
+                        isError = !maxRollIsValid,
+                        supportingText = {
+                            if (!maxRollIsValid) {
+                                Text(stringResource(R.string.must_be_num))
+                            }
+                        }
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        TextButton(
+                            onClick = hideSettings,
+                            modifier = Modifier.padding(8.dp),
+                        ) {
+                            Text(stringResource(R.string.cancel))
+                        }
+                        TextButton(
+                            onClick = {
+                                if (maxRollIsValid) {
+                                    setNewMaxRoll(maxRollStr.toInt())
+                                    hideSettings()
+                                }
+                            },
+                            enabled = maxRollIsValid,
+                            modifier = Modifier.padding(8.dp),
+                        ) {
+                            Text(stringResource(R.string.ok))
+                        }
+                    }
+                }
+            }
+        }
+    )
 }
 @Preview(showBackground = true)
 @Composable
 fun SettingsPreview() {
     DemoDiceTheme {
-        Settings(hideSettings = {})
+        SettingsDialog(
+            hideSettings = {},
+            setNewMaxRoll = {},
+        )
     }
 }
