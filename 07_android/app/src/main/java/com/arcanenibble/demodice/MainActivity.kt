031fbd0a14775a1912165a72742ee8cce6eaeafc
R <rqou@berkeley.edu>
2025-04-19 01:29:20 +0100

    Implement bare minimum dice functionality

--- 07_android/app/src/main/java/com/arcanenibble/demodice/MainActivity.kt
+++ 07_android/app/src/main/java/com/arcanenibble/demodice/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.arcanenibble.demodice.ui.theme.DemoDiceTheme
 
@@ -19,11 +28,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DemoDiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    DiceApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +39,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DiceApp(modifier: Modifier = Modifier) {
+    var diceRoll by remember { mutableStateOf(0) }
 
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {
+                diceRoll = (1..20).random()
+            }
+        ) {
+            Text(
+                if (diceRoll == 0) {
+                    stringResource((R.string.roll))
+                } else {
+                    diceRoll.toString()
+                }
+            )
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DiceAppPreview() {
     DemoDiceTheme {
-        Greeting("Android")
+        DiceApp()
     }
 }
